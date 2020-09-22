fa8467061225032764383934ba3142f147719e76
Maximilian Fischer <maximilian.fischer1991@gmail.com>
2020-09-22 11:33:49 +0200

    added example json code

--- src/Application.cpp
+++ src/Application.cpp
@@ -19,8 +19,11 @@ int main()
 
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    CROW_ROUTE(app, "/json")
+    ([]{
+        crow::json::wvalue x;
+        x["message"] = "Hello, World!";
+        return x;
     });
 
     app.port(18080).multithreaded().run();
