bc5eefb0e6d30a357f7a4037cdde4bf9615fb3dd
Maximilian Fischer <maximilian.fischer1991@gmail.com>
2020-09-23 21:21:50 +0200

    added json magic to return board

--- src/BusinessLogic/BoardManager.cpp
+++ src/BusinessLogic/BoardManager.cpp
@@ -1,7 +1,12 @@
 #include "BoardManager.hpp"
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+#include <iostream>
 
 using namespace Prog3::BusinessLogic;
 using namespace Prog3::Repository;
+using namespace rapidjson;
 
 BoardManager::BoardManager(DataAccessIf & givenDataAccess):
     dataAccess(givenDataAccess)
@@ -15,5 +20,39 @@ BoardManager::~BoardManager()
 std::string BoardManager::getBoard()
 {
     Model::Board board = dataAccess.getBoard();
-    return "\"board\": \"name\"";
+
+    Document document;
+    document.SetObject();
+    Document::AllocatorType& allocator = document.GetAllocator();
+
+    Value jsonBoardTitle(board.getTitle().c_str(), allocator);
+    Value jsonColumns(kArrayType);
+    
+    for (auto column : board.getColumns())
+    {
+        Value singleJsonColumn(kObjectType);
+        singleJsonColumn.AddMember("id", column.id, allocator);
+        singleJsonColumn.AddMember("name", Value(column.name.c_str(), allocator), allocator);
+        singleJsonColumn.AddMember("position", column.position, allocator);
+
+        Value jsonItems(kArrayType);
+        for (auto item : column.items)
+        {
+            Value itemString(item.c_str(), allocator);
+            jsonItems.PushBack(itemString, allocator);
+        }
+        
+        singleJsonColumn.AddMember("items", jsonItems, allocator);
+        jsonColumns.PushBack(singleJsonColumn, allocator);
+    }
+
+    document.AddMember("title", jsonBoardTitle, allocator);
+    document.AddMember("columns", jsonColumns, allocator);
+
+    StringBuffer buffer;
+	Writer<StringBuffer> writer(buffer);
+	document.Accept(writer);
+
+
+    return buffer.GetString();
 }
