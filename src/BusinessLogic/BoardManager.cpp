f8d34e81d349de9d777f2bf7a1de371bab8daf9a
Maximilian Fischer <maximilian.fischer1991@gmail.com>
2020-09-23 15:37:01 +0200

    added dependency injection to BoardManager

---
+++ src/BusinessLogic/BoardManager.cpp
@@ -0,0 +1,19 @@
+#include "BoardManager.hpp"
+
+using namespace Prog3::BusinessLogic;
+using namespace Prog3::Repository;
+
+BoardManager::BoardManager(DataAccessIf& givenDataAccess):
+    dataAccess(givenDataAccess)
+{
+}
+
+BoardManager::~BoardManager()
+{
+}
+
+std::string BoardManager::getBoard()
+{
+    Model::Board board = dataAccess.getBoard();
+    return "myJsonBoard :)";
+}
---
+++ src/BusinessLogic/BoardManager.hpp
@@ -0,0 +1,15 @@
+#include "Repository/DataAccessIf.hpp"
+
+namespace Prog3 { namespace BusinessLogic {
+class BoardManager
+{
+private:
+    Prog3::Repository::DataAccessIf& dataAccess;
+
+public:
+    BoardManager(Prog3::Repository::DataAccessIf& givenDataAccess);
+    ~BoardManager();
+
+    std::string getBoard();
+};
+}}
