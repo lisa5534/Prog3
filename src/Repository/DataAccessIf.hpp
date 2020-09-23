f1c6c01bcc807b256eddd79355388a5170ff66f2
Maximilian Fischer <maximilian.fischer1991@gmail.com>
2020-09-23 14:52:02 +0200

    added interface for data access layer

---
+++ src/Repository/DataAccessIf.hpp
@@ -0,0 +1,15 @@
+
+#include "Model/Board.hpp"
+
+namespace Prog3 { namespace Repository {
+
+class DataAccessIf
+{
+public:
+    ~DataAccessIf() {}
+
+    virtual Prog3::Repository::Model::Board getBoard() = 0;
+
+};
+
+}}
