725e1d81bb0e2c69a2391324ea79bd5cee651175
Maximilian Fischer <maximilianfischer@Christinas-MBP.fritz.box>
2020-09-26 19:32:55 +0200

    refactored BoardManager

--- src/Controller/BoardManager.cpp
+++ src/Controller/BoardManager.cpp
@@ -6,7 +6,9 @@
 
 using namespace Prog3::Controller;
 using namespace Prog3::Repository;
+using namespace Prog3::Model;
 using namespace rapidjson;
+using namespace std;
 
 BoardManager::BoardManager(RepositoryIf & givenRepository):
     repository(givenRepository)
@@ -19,35 +21,17 @@ BoardManager::~BoardManager()
 
 std::string BoardManager::getBoard()
 {
-    Model::Board board = repository.getBoard();
+    Board board = repository.getBoard();
 
     Document document;
     document.SetObject();
     Document::AllocatorType& allocator = document.GetAllocator();
 
-    Value jsonBoardTitle(board.getTitle().c_str(), allocator);
-    Value jsonColumns(kArrayType);
-    
-    for (auto column : board.getColumns())
-    {
-        Value singleJsonColumn(kObjectType);
-        singleJsonColumn.AddMember("id", column.id, allocator);
-        singleJsonColumn.AddMember("name", Value(column.name.c_str(), allocator), allocator);
-        singleJsonColumn.AddMember("position", column.position, allocator);
+    Value boardTitle(board.getTitle().c_str(), allocator);
+    Value columns = getJson(board.getColumns(), allocator);
 
-        Value jsonItems(kArrayType);
-        for (auto item : column.items)
-        {
-            Value itemString(item.c_str(), allocator);
-            jsonItems.PushBack(itemString, allocator);
-        }
-        
-        singleJsonColumn.AddMember("items", jsonItems, allocator);
-        jsonColumns.PushBack(singleJsonColumn, allocator);
-    }
-
-    document.AddMember("title", jsonBoardTitle, allocator);
-    document.AddMember("columns", jsonColumns, allocator);
+    document.AddMember("title", boardTitle, allocator);
+    document.AddMember("columns", columns, allocator);
 
     StringBuffer buffer;
 	Writer<StringBuffer> writer(buffer);
@@ -55,4 +39,36 @@ std::string BoardManager::getBoard()
 
 
     return buffer.GetString();
+}
+
+Value BoardManager::getJson(vector<Column> givenColumns, Document::AllocatorType& allocator)
+{
+    Value columns(kArrayType);
+    for (auto column : givenColumns)
+    {
+        Value jsonColumn(kObjectType);
+        jsonColumn.AddMember("id", column.id, allocator);
+        jsonColumn.AddMember("name", Value(column.name.c_str(), allocator), allocator);
+        jsonColumn.AddMember("position", column.position, allocator);
+
+        Value items = getJson(column.items, allocator);
+        jsonColumn.AddMember("items", items, allocator);
+
+        columns.PushBack(jsonColumn, allocator);
+    }
+
+    return columns;
+}
+
+Value BoardManager::getJson(vector<string> givenItems, Document::AllocatorType& allocator)
+{
+    Value items(kArrayType);
+
+    for (auto item : givenItems)
+    {
+        Value jsonItem(item.c_str(), allocator);
+        items.PushBack(jsonItem, allocator);
+    }
+
+    return items; 
 }
--- src/Controller/BoardManager.hpp
+++ src/Controller/BoardManager.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
 #include "Repository/Repository.hpp"
+#include "rapidjson/document.h"
+#include <list>
 
 namespace Prog3 { namespace Controller {
 class BoardManager
 {
 private:
     Prog3::Repository::RepositoryIf & repository;
+
+    rapidjson::Value getJson(std::vector<std::string> givenItems, rapidjson::Document::AllocatorType& allocator);
+    rapidjson::Value getJson(std::vector<Prog3::Model::Column> givenColumns, rapidjson::Document::AllocatorType& allocator);
 
 public:
     BoardManager(Prog3::Repository::RepositoryIf & givenRepository);
